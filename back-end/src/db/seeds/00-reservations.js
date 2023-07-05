ad561e86f0c1502e841ffefae277456cc7c1396b
Raciel <rcruz0708@gmail.com>
2023-07-05 16:56:40 -0400

    fixing seeding error

--- back-end/src/db/seeds/00-reservations.js
+++ back-end/src/db/seeds/00-reservations.js
@@ -9,6 +9,7 @@ const reservations = require("./00-reservations.json");
 
 //exporting the seed function
 exports.seed = function (knex) {
+  console.log("Seeding reservations table...");
   //truncate the "reservations" table and restart the identity column
   return knex
     .raw("TRUNCATE TABLE reservations RESTART IDENTITY CASCADE")
