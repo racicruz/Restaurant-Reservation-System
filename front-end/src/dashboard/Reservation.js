115f8bca68d6d81a41fe4ef2d801ed60ae354f43
Raciel <rcruz0708@gmail.com>
2023-07-11 13:54:23 -0400

    all frontend tests passing for US-06

--- front-end/src/dashboard/Reservation.js
+++ front-end/src/dashboard/Reservation.js
@@ -12,6 +12,37 @@ const Reservation = ({ reservation }) => {
       status,
     } = reservation;
 
+    const statusElement = 
+      status === "booked" ? (
+        <div 
+          style={{ cursor: "default" }}
+          className='btn border border-success rounded text-success'
+        >
+          Booked
+        </div>
+      ) : status === "seated" ? (
+        <div 
+          style={{ cursor: "default" }}
+          className='btn border border-warning rounded text-warning'
+        >
+          Seated
+        </div>
+      ) : status === "cancelled" ? (
+        <div 
+          style={{ cursor: "default" }}
+          className='btn border border-danger rounded text-danger'
+        >
+          Cancelled
+        </div>
+      ) : (
+        <div 
+          style={{ cursor: "default" }}
+          className='btn border border-muted rounded text-muted'
+        >
+          Finished
+        </div>
+      );
+
 
     return (
     <div className="card mb-3 shadow-sm">
@@ -70,8 +101,8 @@ const Reservation = ({ reservation }) => {
               data-reservation-id-status={reservation.reservation_id}
               className="col card-text mb-0 pl-0"
             >
+              {statusElement}
             </div>
-
             
           </div>
         </div>
